afbe83230686fbc07ccd7e575d2417788e79e5b1
Hannah Kok <hannahkok93@outlook.com>
2023-04-21 22:06:32 +1000

    add router dom components

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,48 @@ import Nav from "./components/Nav/Nav";
 import HomePage from "./containers/HomePage/HomePage";
 import ProductPage from "./containers/ProductPage/ProductPage";
 import CartPage from "./containers/CartPage/CartPage";
+import { getActivewearById, getAllActivewear } from "./services/activewear";
 
 function App() {
+    const [items, setItems] = useState(null);
+    const [updated, setUpdated] = useState(0);
+
+    useEffect(() => {
+        const wrapper = async () => {
+            const allItems = await getAllActivewear();
+            setItems(allItems);
+        };
+        wrapper();
+    }, [updated]);
+    // console.log(items, "items");
+
     return (
         <div className={styles.App}>
             <BrowserRouter>
                 <Nav />
                 <Routes>
-                    <Route path="/products" element={<HomePage />} />
-                    <Route path="/products/:id" />
-                    <Route path="/cart" element={<CartPage />} />
-                    <Route path="/testproduct" element={<ProductPage />} />
+                    <Route
+                        path="/"
+                        element={
+                            <HomePage
+                                items={items}
+                                setItems={setItems}
+                                updated={updated}
+                                setUpdated={setUpdated}
+                            />
+                        }
+                    />
+                    <Route
+                        path="/products/:id"
+                        element={
+                            <ProductPage
+                                updated={updated}
+                                setUpdated={setUpdated}
+                            />
+                        }
+                    />
+                    <Route path="/cart" />
+                    {/* <Route path="/testproduct" element={<ProductPage />} /> */}
                 </Routes>
             </BrowserRouter>
         </div>
