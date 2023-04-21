d58d3284fe7c3e174f0ee6bd9cb68db9527664a5
Hannah Kok <hannahkok93@outlook.com>
2023-04-21 22:00:15 +1000

    add props, navlink

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,15 +1,18 @@
 import styles from "./ProductCard.module.scss";
 import { NavLink } from "react-router-dom";
-import { useState, useEffect } from "react";
 
 const ProductCard = ({ id, img, brand, name, price }) => {
     return (
-        <div>
-            <NavLink to={`products/${id}`}>
-                <img src={img} alt="product" />
+        <div className={styles.ProductCard}>
+            <NavLink to={`/products/${id}`}>
+                <img
+                    className={styles.ProductCard_Img}
+                    src={img}
+                    alt="product"
+                />
                 <p>{brand}</p>
                 <p>{name}</p>
-                <p>{price}</p>
+                <p>${price}.00</p>
             </NavLink>
         </div>
     );
