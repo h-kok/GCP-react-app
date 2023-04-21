4ce921045ecdd20702d73500f6636c850040ec4b
Hannah Kok <hannahkok93@outlook.com>
2023-04-21 22:01:02 +1000

    add product cards, carousel, props

--- src/containers/HomePage/HomePage.jsx
+++ src/containers/HomePage/HomePage.jsx
@@ -1,11 +1,32 @@
-import ProductList from "../ProductList/ProductList";
+import ProductCard from "../../components/ProductCard/ProductCard";
+import CarouselContainer from "../CarouselContainer/CarouselContainer";
 import styles from "./HomePage.module.scss";
 
-const HomePage = () => {
+const HomePage = ({ items }) => {
+    // console.log(items, "homepage");
     return (
         <div className={styles.HomePage}>
-            <h1>My Products</h1>
-            <ProductList />
+            <h1>ActiveWear</h1>
+            <h3>Shop the latest styles</h3>
+            <CarouselContainer items={items} />
+            <div className={styles.ProductList}>
+                {items ? (
+                    items.map((item) => {
+                        return (
+                            <ProductCard
+                                key={item.id}
+                                id={item.id}
+                                img={item.image[0]}
+                                brand={item.brand}
+                                name={item.name}
+                                price={item.price}
+                            />
+                        );
+                    })
+                ) : (
+                    <p>Items loading...</p>
+                )}
+            </div>
         </div>
     );
 };
