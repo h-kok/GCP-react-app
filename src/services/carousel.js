a02bc1ac7cb2cf7867bb8a6160a6d837dedf19fb
Hannah Kok <hannahkok93@outlook.com>
2023-04-25 11:55:13 +1000

    clean up, formatting

--- src/services/carousel.js
+++ src/services/carousel.js
@@ -4,12 +4,12 @@ export const getCarouselItems = async (array) => {
         Math.floor(Math.random() * array.length)
     );
     const randomItems = randomNumArr.map((num) => array[num]);
-    // console.log(randomItems);
+
     const carouselItems = randomItems.map((item) => {
         const { image, id, ...rest } = item;
         const newObj = { image, id };
         return newObj;
     });
-    // console.log(carouselItems, "carousel items");
+
     return carouselItems;
 };
