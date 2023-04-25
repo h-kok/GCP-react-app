0400e2094f982a4a594be431698708aa7208b916
Hannah Kok <hannahkok93@outlook.com>
2023-04-25 11:51:01 +1000

    add state to track item quantity and for error handling messages, add handle add to cart function incl creating cart item, handling duplicate cart items, handling item unavailable

--- src/containers/ProductPage/ProductPage.jsx
+++ src/containers/ProductPage/ProductPage.jsx
@@ -8,53 +8,74 @@ import unheart from "../../assets/unheart.png";
 import heart from "../../assets/heart.png";
 import { updateActivewear } from "../../services/activewear";
 
-const ProductPage = ({ updated, setUpdated }) => {
+const ProductPage = ({ cartItems }) => {
     const { id } = useParams();
     const [item, setItem] = useState(null);
-    // console.log(item, "item");
     const [like, setLike] = useState(false);
-    const [unavailable, setUnavailable] = useState(false);
     const [index, setIndex] = useState(0);
-    // console.log(like);
-
-    const handleClickLike = async () => {
-        await updateActivewear(id, { favourited: !like });
-        setLike(!like);
-    };
-
-    const handleClickBuy = async () => {
-        const data = await getActivewearById(id);
-        // console.log(data.quantity);
-        try {
-            if (data.quantity < 1) throw "Sorry, this item is unavailable";
-        } catch (err) {
-            setUnavailable(true);
-        }
-        await updateQuantity(id, -1);
-    };
-
-    const handleColourChange = (e) => {
-        const colour = e.target.value;
-        // console.log(optionVal, "option");
-        const findIndex = item.colour.findIndex((i) => i === colour);
-        setIndex(findIndex);
-        //find value or index? of colour selection so can display on cart page.
-    };
-
-    const handleSizeChange = (e) => {
-        const size = e.target.value;
-        //find value or index? of size selection so can display on cart page.
-    };
+    const [quantity, setQuantity] = useState(null);
+    const [addMsg, setAddMsg] = useState(false);
+    const [sizeMsg, setSizeMsg] = useState(false);
+    const [duplicateMsg, setDuplicateMsg] = useState(false);
 
     useEffect(() => {
         const wrapper = async () => {
             const data = await getActivewearById(id);
-            // console.log(data, "data");
             setItem(data);
-            setIndex(0);
+            setQuantity(data.quantity);
         };
         wrapper();
-    }, [updated, id]);
+        console.log("render, product page");
+    }, [id]);
+
+    const handleClickLike = () => {
+        updateActivewear(id, { favourited: !like });
+        setLike(!like);
+    };
+
+    const handleClickAdd = () => {
+        if (size.value === "Select a size") {
+            setSizeMsg(true);
+        } else {
+            const createCartItem = {
+                id: item.id,
+                image: item.image[index],
+                name: item.name,
+                colour: colour.value,
+                size: size.value,
+                quantity: 1,
+                price: item.price,
+            };
+
+            const checkDuplicate = cartItems.findIndex(
+                (i) =>
+                    `${i.id}${i.colour}${i.size}` ===
+                    `${createCartItem.id}${createCartItem.colour}${createCartItem.size}`
+            );
+
+            if (checkDuplicate < 0 && quantity > 0) {
+                cartItems.push(createCartItem);
+                localStorage.setItem("cartItems", JSON.stringify(cartItems));
+                setAddMsg(true);
+                updateQuantity(id, -1);
+                setQuantity(quantity - 1);
+            } else {
+                setDuplicateMsg(true);
+            }
+        }
+    };
+
+    const handleColourChange = (e) => {
+        const colour = e.target.value;
+        const findIndex = item.colour.findIndex((i) => i === colour);
+        setIndex(findIndex);
+        setAddMsg(false);
+    };
+
+    const handleOptionChange = () => {
+        setAddMsg(false);
+        setSizeMsg(false);
+    };
 
     return item ? (
         <div className={styles.Product}>
@@ -62,32 +83,49 @@ const ProductPage = ({ updated, setUpdated }) => {
                 <img
                     className={styles.Product_Img}
                     src={item.image[index] ?? "Image unavailable"}
-                    alt="product image"
+                    alt={item.name}
                 />
             </div>
             <div>
                 <p>{item.brand ?? "Brand unavailable"}</p>
                 <p>{item.name ?? "Name unavailable"}</p>
                 <p>${item.price ?? "Price unavailabe"}.00</p>
-                <p>Colour:</p>
-                <select name="" id="" onChange={handleColourChange}>
+                <label htmlFor="colour">Colour: </label>
+                <select
+                    name="colour"
+                    id="colour"
+                    onChange={handleColourChange}
+                    required
+                >
                     {item.colour &&
                         item.colour.map((item) => {
                             return <Option value={item} key={item} />;
                         })}
                 </select>
-                <p>Size:</p>
-                <select name="" id="">
+                <label htmlFor="size">Size: </label>
+                <select
+                    name="size"
+                    id="size"
+                    defaultValue="Select a size"
+                    onChange={handleOptionChange}
+                    required
+                >
+                    <option value="Select a size" hidden disabled>
+                        Select a size
+                    </option>
                     {item.size &&
                         item.size.map((item) => {
                             return <Option value={item} key={item} />;
                         })}
                 </select>
+                {sizeMsg && <p>Please select a size.</p>}
+                {duplicateMsg && <p>This item is already in your cart.</p>}
                 <div>
                     <Button
-                        onClick={handleClickBuy}
+                        onClick={handleClickAdd}
                         className={styles.Product_BuyBtn}
                         value="Add to Cart"
+                        disabled={quantity < 0}
                     />
                     <Button
                         onClick={handleClickLike}
@@ -100,18 +138,15 @@ const ProductPage = ({ updated, setUpdated }) => {
                             />
                         }
                     />
-                    <p>
-                        {!unavailable
-                            ? "Your item was added sucessfully!"
-                            : "Sorry, this item is unavailable"}
-                    </p>
+                    {addMsg && <p>Your item was added successfully!</p>}
+                    {quantity < 0 && <p>Sorry, this item is unavailable</p>}
                 </div>
             </div>
             <p>Product Details</p>
             <div></div>
         </div>
     ) : (
-        <p>Sorry, item not found.</p>
+        <p>Loading item...</p>
     );
 };
 
