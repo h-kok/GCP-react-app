d82db4b792749982c48fce666c22c2e8b4014ee4
Hannah Kok <hannahkok93@outlook.com>
2023-04-25 11:41:01 +1000

    add state for increment and decrement btns and update quantities in database, add navlink to cart imgs, add error handling if item not available, add handle remove item from cart function

--- src/components/CartProductCard/CartProductCard.jsx
+++ src/components/CartProductCard/CartProductCard.jsx
@@ -1,32 +1,96 @@
 import styles from "./CartProductCard.module.scss";
 import Button from "../Button/Button";
+import { getActivewearById, updateQuantity } from "../../services/activewear";
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
 
-const CartProductCard = () => {
+const CartProductCard = ({
+    id,
+    extraId,
+    image,
+    brand,
+    name,
+    colour,
+    size,
+    quantity,
+    price,
+    cartItems,
+    setCartItems,
+}) => {
+    const [showItem, setShowItem] = useState(true);
+    const [count, setCount] = useState(quantity);
+    const [unavailable, setUnavailable] = useState(false);
+
+    const handleRemove = () => {
+        updateQuantity(id, +count);
+
+        const findIndex = cartItems.findIndex(
+            (i) => `${i.id}${i.colour}${i.size}` === extraId
+        );
+        cartItems.splice(findIndex, 1);
+        localStorage.setItem("cartItems", JSON.stringify(cartItems));
+        setCartItems(JSON.parse(localStorage.getItem("cartItems")));
+
+        if (!cartItems.length) {
+            localStorage.removeItem("cartItems");
+        }
+        setShowItem(false);
+    };
+
+    const handleIncrement = async () => {
+        const data = await getActivewearById(id);
+        if (data.quantity === 0) {
+            setUnavailable(true);
+        } else {
+            setCount(count + 1);
+            updateQuantity(id, -1);
+        }
+    };
+
+    const handleDecrement = () => {
+        setUnavailable(false);
+        if (count > 1) {
+            setCount(count - 1);
+            updateQuantity(id, +1);
+        }
+    };
+
     return (
-        <div className={styles.Product}>
-            <div>
-                <img src="" alt="product image" />
+        showItem && (
+            <div className={styles.Product}>
+                <NavLink to={`/products/${id}`}>
+                    <div>
+                        <img
+                            className={styles.Product_Img}
+                            src={image}
+                            alt="product image"
+                        />
+                    </div>
+                </NavLink>
+                <div>
+                    <p>{brand}</p>
+                    <p>{name}</p>
+                    <p>Colour: {colour}</p>
+                    <p>Size: {size}</p>
+                    <p>
+                        Quantity: <Button value="-" onClick={handleDecrement} />
+                        <span>{count}</span>
+                        <Button value="+" onClick={handleIncrement} />
+                    </p>
+                    {unavailable && <p>Maximum quantity reached.</p>}
+                    <p>${price * quantity}.00</p>
+                </div>
+                <div>
+                    <Button
+                        value={[
+                            <img src="https://img.icons8.com/ios/20/null/trash--v1.png" />,
+                            "Remove",
+                        ]}
+                        onClick={handleRemove}
+                    />
+                </div>
             </div>
-            <div>
-                <p>Name</p>
-                <p>Colour</p>
-                <p>Size</p>
-                <p>Quantity</p>
-            </div>
-            <div>
-                <Button value="-" />
-                <span>0</span>
-                <Button value="+" />
-            </div>
-            <div>
-                <Button
-                    value={[
-                        <img src="https://img.icons8.com/ios/20/null/trash--v1.png" />,
-                        "Remove",
-                    ]}
-                />
-            </div>
-        </div>
+        )
     );
 };
 
